ff054066261001b119605b693463f28437b06c62
Alexandre Perdomo <aperdomo@free.fr>
2020-02-22 00:26:13 +0100

    last modifications

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,38 @@
 class PagesController < ApplicationController
+
   def home; end
 
-  def election; end
+  def election;
+    url = default_url
+    route = url + "/lists"
+    response = RestClient.get(route, headers={})
+    if response.code == 200
+      @lists = JSON.parse response
+    else 
+      @lists = nil
+    end
+  end
+
+  def list
+    url = default_url
+    route = url + "/lists/#{params[:name]}"
+    response = RestClient.get(route, headers={})
+    if response.code == 200
+      @list = JSON.parse response
+    else 
+      @list = nil
+    end
+  end
 
   def doleances; end
+
+  def cgu; end
+
+  private
+
+  def default_url
+  	return "localhost:3000/api/v1" if Rails.env == "development"
+
+  	"https://democracy-api.herokuapp.com/api/v1"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get 'election-municipale-2020', to: "pages#election"
-  get 'doléances', to: "pages#doleances"
+  get '/election-municipale-2020', to: "pages#election"
+  get '/election-municipale-2020/:name', to: "pages#list"
+  get '/doléances', to: "pages#doleances"
+  get '/mentions-légales', to: "pages#cgu"
 end
