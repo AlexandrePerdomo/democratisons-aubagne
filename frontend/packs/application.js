55d26c451cd989518b1e39b7a0dd13ab43f15e42
Alexandre Perdomo <aperdomo@free.fr>
2020-03-09 11:21:42 +0100

    configuration for rails ujs

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -2,3 +2,5 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 import "../../node_modules/bootstrap/dist/js/bootstrap";
 
 import "./application.pcss";
+
+require("@rails/ujs").start();
